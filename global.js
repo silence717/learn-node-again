350b5d39fcd177301dc2ba2a9556fb1f80c772bb
fang.yang <yangfang2028@126.com>
2020-07-21 09:51:39 +0800

    ✨ file system

---
+++ global.js
@@ -0,0 +1,12 @@
+// console.log(global);
+
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+// }, 3000)
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__diranme);
+// console.log(__finename);
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['silence', 'hailey', 'geria'];
+const ages = ['18', '17', '16'];
+
+module.exports = {
+    people,
+    ages
+};
